ba098938f644a0d3d3f7d8dd79a786829fe41178
Andy Holt <andyholt85@gmail.com>
2020-11-30 04:19:59 -0700

    set up initial project structure

---
+++ src/components/Game.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="Game">
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/components/Game.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Game from './Game';
+
+test('Game renders', () => {
+  render(<Game />);
+});
